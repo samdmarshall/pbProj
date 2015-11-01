f63ba5402ed3e4fe777aa8295dc4d74fbc5496f3
Samantha Marshall <me@samdmarshall.com>
2015-10-31 22:06:49 -0400

    cleaning up imports

--- pbProj/pbProj.py
+++ pbProj/pbProj.py
@@ -1,7 +1,4 @@
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pbPlist'))
-import pbPlist
+from pbPlist import pbPlist
 
 from .PBX_Constants import *
 from .PBX_Lookup import *
